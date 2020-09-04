bf1c8b44f670c88533bad700c77695baf137e0da
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-09-04 07:17:44 +0400

    MVN001 - init project

---
+++ service/src/main/java/com/maslov/maven/service/PersonService.java
@@ -0,0 +1,20 @@
+package com.maslov.maven.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.maslov.maven.core.Person;
+
+public class PersonService {
+
+    public Person parse(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person person = objectMapper.readValue(json, Person.class);
+        return person;
+    }
+
+    public String convert(Person person) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(person);
+    }
+
+}
